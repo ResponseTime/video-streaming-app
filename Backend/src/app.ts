2981c36467efd348f3299cb9507f6bd9836631e4
responsetime <aayush.bhattacharjee2002@gmail.com>
2023-12-28 16:58:36 +0530

    created local video processing service

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,12 +1,31 @@
 import express from "express";
+import ffmpeg from "fluent-ffmpeg";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+app.use(express.json())
 
-app.get("/", (req, res) => {
-    res.json({success:true})
-})
+app.post("/process-video", (req, res) => {
+    const filePath = req.body.filePath;
+    const outFile = req.body.outFile;
 
-app.listen(port,()=>{
-    console.log("listening on port 3000")
+    if (!filePath || !outFile) {
+        res.status(400).json({ message: "Invalid file" });
+    }
+    ffmpeg(filePath)
+        .outputOption("-vf", "scale=-1:360")
+        .on("end", (err) => {
+            res.status(200).json({
+                message: "Video finished"
+            });
+        })
+        .on("error", (err) => {
+            console.log(err.message);
+            res.status(500).json({ message: err.message });
+        })
+        .save(outFile);
+});
+
+app.listen(port, () => {
+    console.log("listening on port 3000");
 });
